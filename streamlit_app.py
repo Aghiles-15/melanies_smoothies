c0060c849681317bdd283b7f5dbeea7baf76f766
Aghiles-15 <aghilpub@gmail.com>
2024-06-21 14:27:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,16 @@
+
+
   # Importer les packages nécessaires
 import streamlit as st
 import pandas as pd  # Ajout de l'importation de pandas
 from snowflake.snowpark.session import Session
 from snowflake.snowpark.functions import col
-import requests  # Déplacer l'instruction d'importation en haut
+import requests
 
 # Définir la fonction pour obtenir une session active
 def get_active_session():
     connection_parameters = {
-         "account": "BAMQRAZ-AY21624",
+        "account": "BAMQRAZ-AY21624",
         "user": "AGHIL",
         "password": "123Azerty123**",
         "role": "SYSADMIN",
@@ -28,12 +30,10 @@ st.write("Le nom sur votre smoothie sera :", name_on_order)
 
 # Utiliser la session active
 session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')).to_pandas()
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
-pd_df=my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')).to_pandas()
+
+# Afficher le DataFrame Pandas dans l'application Streamlit
+st.dataframe(my_dataframe)
 
 ingredients_list = st.multiselect('Choisissez les 5 meilleurs ingrédients :', my_dataframe['FRUIT_NAME'], max_selections=5)
 
@@ -42,13 +42,17 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
       
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on = my_dataframe.loc[my_dataframe['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
       
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen.lower()}")
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
+        if fruityvice_response.status_code == 200:
+            fv_df = pd.json_normalize(fruityvice_response.json())
+            st.dataframe(fv_df)
+        else:
+            st.write("Information nutritionnelle non disponible pour ", fruit_chosen)
+    
     st.write(ingredients_string)
 
     my_insert_stmt = f"INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES ('{ingredients_string.strip()}', '{name_on_order}')"
